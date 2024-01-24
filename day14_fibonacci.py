349f479df6bce70c3da06ecb0f50c7db1f7bfb13
Svetlana Lebedeva <slebedeva@gmail.com>
2024-01-24 11:13:35 +0100

    replaced recursion with closed form solution

--- day14_fibonacci.py
+++ day14_fibonacci.py
@@ -23,15 +23,20 @@ def fib(n: int):
     if not isinstance(n, int) or n<=0:
         print('provide positive integer >= 1')
         return None
-    """ classbook recursive function """
+    # classbook recursive function - very bad for n=100!
     # special / conditions to stop
-    if n==1:
-        return 1
-    elif n==2:
-        return 1
-    # recursion: call itself
-    else:
-        return fib(n-1) + fib(n-2) #sum of two preceding ones
+    #if n==1:
+    #    return 1
+    #elif n==2:
+    #    return 1
+    ## recursion: call itself
+    #else:
+    #    return fib(n-1) + fib(n-2) #sum of two preceding ones
+
+    # closed form solution: O(n) to print n numbers
+    # Binet's formula (see Wiki)
+    golden_ratio = (5**0.5+1)/2
+    return round((golden_ratio**n)/(5**0.5))
 
 if __name__=="__main__":
     try:
