66269ed49abb8ea33b4557f8dc64f7e6e9027a1c
Svetlana Lebedeva <slebedeva@gmail.com>
2024-01-24 23:28:34 +0100

    changed back to recursion and learned memoization and dynamic programming

--- day14_fibonacci.py
+++ day14_fibonacci.py
@@ -1,4 +1,5 @@
 # Write a program to print the first n numbers of a Fibonacci sequence
+import ipdb
 def print_fibonacci(n: int):
     """
     Prints first n numbers of Fibonacci sequence.
@@ -15,31 +16,36 @@ def print_fibonacci(n: int):
     if not isinstance(n, int):
         print('provide positive integer >= 1')
     else:
-        print([fib(i) for i in range(1,n+1)])
+        # TODO optimize this to return to memo too
+        print([fib(i,{}) for i in range(1,n+1)])
     return None
 
-def fib(n: int):
+def fib(n: int, memo: dict[int, int]):
     # if invalid input
     if not isinstance(n, int) or n<=0:
         print('provide positive integer >= 1')
         return None
-    # classbook recursive function - very bad for n=100!
-    # special / conditions to stop
-    #if n==1:
-    #    return 1
-    #elif n==2:
-    #    return 1
-    ## recursion: call itself
-    #else:
-    #    return fib(n-1) + fib(n-2) #sum of two preceding ones
+
+    # we can keep recursion but need to use dynamic programming
+    # to avoid recalculating Fibonacci for the same number many many times
+    # this is called memoization (sic) - store already calculated values
+    # memo, dict to store values, needs to be argument of the recursive function
+    if n in memo:
+        return memo[n]
+    if n in [1,2]:
+        return 1
+    # do not forget to record this into dict
+    memo[n] = fib(n-1, memo) + fib(n-2, memo)
+    return memo[n]
 
     # closed form solution: O(n) to print n numbers
     # Binet's formula (see Wiki)
-    golden_ratio = (5**0.5+1)/2
-    return round((golden_ratio**n)/(5**0.5))
+    #golden_ratio = (5**0.5+1)/2
+    #return round((golden_ratio**n)/(5**0.5))
 
 if __name__=="__main__":
     try:
+        memo = {}
         print_fibonacci(int(input('enter how many numbers from Fibonacci to print:')))
     except ValueError:
         print('provide integer >=1')
