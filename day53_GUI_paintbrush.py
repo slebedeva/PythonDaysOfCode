84c058d66aaa2946973ae0cb385b05f1c476e026
Svetlana Lebedeva <slebedeva@gmail.com>
2024-03-04 18:49:36 +0100

    added trackpads for color selection and changed the starting window to white

--- day53_GUI_paintbrush.py
+++ day53_GUI_paintbrush.py
@@ -12,7 +12,7 @@ import numpy as np
 # mouse callback function
 def draw_circle(event,x,y,flags,param):
     # take global variables:
-    global ix,iy,drawing,mode
+    global ix,iy,drawing,mode,r,g,b
     # if the user presses the mouse: start drawing
     if event == cv.EVENT_LBUTTONDOWN:
         drawing = True
@@ -20,24 +20,46 @@ def draw_circle(event,x,y,flags,param):
     # if the mouse moves and the button is pressed: continue adding cricles
     elif event == cv.EVENT_MOUSEMOVE:
         if drawing == True:
-            cv.circle(img,(x,y),5,(0,0,255),-1)
+            cv.circle(img,(x,y),5,(r,g,b),-1)
     # if buton is unpressed: stop drawing but still add a circle
     elif event == cv.EVENT_LBUTTONUP:
         drawing = False
-        cv.circle(img,(x,y),5,(0,0,255),-1)
+        cv.circle(img,(x,y),5,(r,g,b),-1)
 
 # initiate
 drawing = False # true if mouse is pressed
 ix,iy = -1,-1 #initial coordinates
 # make a black window
-img = np.zeros((512,512,3), np.uint8)
+#img = np.zeros((512,512,3), np.uint8)
+# or a white one:
+img = np.full((512,512,3),255, np.uint8)
 cv.namedWindow('image')
+
+# create trackbars for color change
+# it still requires callback - in this case, empty
+def nothing(x):
+    pass
+
+cv.createTrackbar('R','image',0,255,nothing)
+cv.createTrackbar('G','image',0,255,nothing)
+cv.createTrackbar('B','image',0,255,nothing)
+# TODO: trackbar for brush size selection
+
 cv.setMouseCallback('image',draw_circle)
 while(1):
     cv.imshow('image',img)
     k = cv.waitKey(1) & 0xFF
 
+    # get current positions of four trackbars
+    r = cv.getTrackbarPos('R','image')
+    g = cv.getTrackbarPos('G','image')
+    b = cv.getTrackbarPos('B','image')
+
     # close window on pressing Esc:
     if k == 27:
         break
 cv.destroyAllWindows()
+
+##############################################
+# add trackbar for color palette
+##############################################
